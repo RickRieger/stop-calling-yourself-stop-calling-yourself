f4cf566f47798b7da2457740d01712e741317a87
Rick Rieger <rickrieger36251@gmail.com>
2021-02-18 23:06:11 -0500

    too easy

---
+++ main.js
@@ -0,0 +1,61 @@
+
+//* task 1
+function whatTheHeck(arbitraryString){
+return arbitraryString + '!!!';
+}
+
+    let answer = whatTheHeck('Are you serious');
+    answer;
+
+
+
+//* task 2
+function whiteSpace(word, wordUp){
+    return word + ' ' + wordUp;
+}
+
+    answer = whiteSpace('WhatIfWeAllStartedCommunicatingWithCamelSpace?','look Mom, space.');
+    answer;
+
+
+//* task 3
+function subtractThreeNums(a,b,c){
+    return a - b - c;
+}
+
+    answer = subtractThreeNums(10,1,2);
+    answer;
+    answer = subtractThreeNums(8,3,5);
+    answer;
+    answer = subtractThreeNums(100,80,25);
+    answer;
+
+
+//* task 4
+function subThreeNumsReverse(a,b,c){
+    return c - b - a;
+}
+
+    answer = subThreeNumsReverse(10,1,2);
+    answer;
+    answer = subThreeNumsReverse(2,1,10);
+    answer;
+
+//*task 5
+function tripleAstring(string){
+    return string+string+string;
+}
+
+    answer = tripleAstring('isThisGoodEnough?');
+    answer;
+
+//task 6
+function squareRoot(num){
+    return Math.sqrt(num);
+}
+    answer = squareRoot(36);
+    answer;
+
+
+    
+//* :)
